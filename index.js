c0566b9287745bad008f7f39db3241843a8136fb
Dong Nguyen <ndaidong@gmail.com>
2024-10-03 09:45:15 +0700

    v0.1.4
    - Fix path import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 
-export { minifyJS as jsminify, minifyCSS as cssminify } from './minifier.js'
+export { minifyJS as jsminify, minifyCSS as cssminify } from './utils/minifier.js'
 
 export { transform as jsify, transformFile as jsbuild } from './utils/jsify.js'
 export { transform as cssify, transformFile as cssbuild } from './utils/cssify.js'
